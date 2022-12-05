ee1a3708f2ace58622abcd9b43148097347a57b2
m-nobinur <mohammadnobinur@gmail.com>
2022-12-06 01:55:05 +0600

    Update

--- next.config.js
+++ next.config.js
@@ -98,3 +98,27 @@ module.exports = withContentlayer(
     assetPrefix: '/techponjika-test/',
   })
 )
+
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+      imagesPublicPath: '/techponjika-test/_next/static/images/',
+    },
+  ],
+  {
+    basePath: '/techponjika-test',
+    assetPrefix: '/techponjika-test/',
+  },
+])
